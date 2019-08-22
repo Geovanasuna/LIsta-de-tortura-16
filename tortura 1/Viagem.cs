518681800168c000098994c1513669c6659c2e9d
geovanasunasan@gmail.com <geovanasunasan@gmail.com>
2019-08-22 19:20:46 -0300

    quero fugir!

---
+++ tortura 1/Viagem.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tortura_1
+{
+    class Viagem
+    {
+        private double distancia;
+        private double tempo;
+        public void  SetDistancia (double d)
+        {
+            distancia = d;
+        }
+        public void  SetTempo (double t)
+        {
+            tempo = t;
+        }
+        public double  GetDistancia()
+        {
+            return distancia;
+        }
+        public double GetTempo()
+        {
+            return tempo;
+        }
+        public double CalcVelocidade()
+        {
+            return distancia / tempo;
+        }
+
+        
+    }
+}
