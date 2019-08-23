88664fafe0b29d20e59ee9b44c5346335736f621
geovanasunasan@gmail.com <geovanasunasan@gmail.com>
2019-08-23 14:59:10 -0300

    balbalbal

---
+++ Satisfação4/Equaçãoi.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Satisfação4
+{
+    class Equaçãoi
+    {
+        private double a, b, c;
+        public void SetABC(double x, double y, double z)
+        {
+            
+            a = x; b = y; c = z;
+        }
+        public double[] GetABC()
+        {
+            double[] v = new double[3];
+            v[0] = a; v[1] = b; v[2] = c;
+            return v;
+        }
+        public void GetABC(out double a, out double b, out double c)
+        {
+            a = this.a; b = this.b; c = this.c;
+        }
+        public double Delta()
+        {
+            return b * b - 4 * a * c;
+        }
+        public bool RaizesReais()
+        {
+            return Delta() >= 0;
+            
+        }
+        public bool X1(out double x)
+        {
+            x = 0;
+            if (RaizesReais() == false || a == 0) return false;
+            x = (-b + Math.Sqrt(Delta())) / (2 * a);
+            return true;
+        }
+        public bool X2(out double x)
+        {
+            x = 0;
+            if (RaizesReais() == false || a == 0) return false;
+            x = (-b - Math.Sqrt(Delta())) / (2 * a);
+            return true;
+        }
+
+    }
+}
---
+++ Satisfação4/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Satisfação4
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CalcularClick(object sender, RoutedEventArgs e)
+        {
+
+            EquacaoIIGrau eq = new EquacaoIIGrau();
+            eq.SetABC(double.Parse(A.Text), double.Parse(B.Text),
+                double.Parse(C.Text));
+            D.Text = eq.Delta().ToString();
+            double r, s;
+            if (eq.X1(out r))
+            {
+                eq.X2(out s);
+                X.Text = r.ToString();
+                Y.Text = s.ToString();
+            }
+            else
+            {
+                X.Text = "Raiz Complexa";
+               Y.Text = "Raiz Complexa";
+                MessageBox.Show("A equação não é do II grau ou não tem raízes reais");
+            }
+
+        }
+    }
+}
